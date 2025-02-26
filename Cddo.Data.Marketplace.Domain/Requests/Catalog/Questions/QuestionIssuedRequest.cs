63558b7ee20b87463c3d7e856b9703878107aa73
Liam Tonner <liam.tonner@agrimetrics.co.uk>
2025-02-26 11:24:00 +0000

    validation issue fixes

--- Cddo.Data.Marketplace.Domain/Requests/Catalog/Questions/QuestionIssuedRequest.cs
+++ Cddo.Data.Marketplace.Domain/Requests/Catalog/Questions/QuestionIssuedRequest.cs
@@ -6,3 +6,11 @@ public class QuestionIssuedRequest : CatalogDataRequestBase
     public int metadataIssuedYear { get; set; }
     public DateTime metadataIssuedDate { get; set; }
 }
+public class QuestionIssuedRequestModel
+{
+    public string? Identifier { get; set; }
+    public string metadataIssuedDay { get; set; }
+    public string metadataIssuedMonth { get; set; }
+    public string metadataIssuedYear { get; set; }
+
+}
