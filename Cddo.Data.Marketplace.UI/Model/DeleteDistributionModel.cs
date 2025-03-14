77bec4aa8d4b698f0adbd9fe6c315ecd6ed893c1
Liam Tonner <liam.tonner@agrimetrics.co.uk>
2025-03-14 16:23:41 +0000

    delete functionality and validation

---
+++ Cddo.Data.Marketplace.UI/Model/DeleteDistributionModel.cs
@@ -0,0 +1,13 @@
+﻿using Agm.Catalog.DotNet.Dto.Models.DataAssets.Profiles.DcatUk.V3_1;
+
+namespace Cddo.Data.Marketplace.UI.Model;
+
+public class DeleteDistribtionModel
+{
+    public string Title { get; set; }
+    public string? Identifier { get; set; }
+    public int DistributionId { get; set; }
+    string? isCheckList { get; set; }
+    string? isCheckAnswers { get; set; }
+    string? isEditMode { get; set; }
+}
